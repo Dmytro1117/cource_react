34bde64419e1b000f0f63e74219c47133b5da461
Dmytro <moiseenkodmitriy@i.ua>
2025-01-06 19:54:34 +0200

    lesson-3

--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./Header.module.css";
-
-export default function Header() {
-  return (
-    <header className={css.container}>
-      <h1 className={css.pageTitle}>Styling React components</h1>
-    </header>
-  );
-}
