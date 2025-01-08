c80c29d006988d0865d62913b084701adb46b836
Dmytro <moiseenkodmitriy@i.ua>
2025-01-08 23:11:37 +0200

    lesson-6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,15 @@
 import UserForm from "../UserForm/UserForm";
-import { useState } from "react";
-import TextInput from "../TextInput/TextInput";
 import css from "./App.module.css";
-import LangSwitcher from "../LangSwitcher/LangSwitcher";
-import OrderForm from "../OrderForm/OrderForm";
 
 export default function App() {
-  const [lang, setLang] = useState("en");
-  const [text, setText] = useState("");
-
-  const handleLangChange = (newLang) => {
-    setLang(newLang);
-  };
-
-  const handleTextChange = (newText) => {
-    setText(newText);
-  };
-
-  const addNewOrder = (newOrder) => {
-    console.log(newOrder);
-  };
-
-  const addNewUser = (newUser) => {
+  const addUser = (newUser) => {
     console.log(newUser);
   };
 
   return (
     <div className={css.container}>
-      <h1>Forms in React</h1>
-      <UserForm onAdd={addNewUser} />
-
-      <OrderForm onOrder={addNewOrder} />
-
-      <TextInput value={text} onUpdate={handleTextChange} />
-      <p>{text}</p>
-
-      <LangSwitcher value={lang} onSelect={handleLangChange} />
-      <p>Current lang: {lang}</p>
+      <h1>Forms with Formik</h1>
+      <UserForm onAdd={addUser} />
     </div>
   );
 }
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,36 +1,91 @@
-import { useId } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import css from "./UserForm.module.css";
 
-export default function UserForm({ onAdd }) {
-  const id = useId();
+const UserSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too short, min 3 letters!")
+    .max(15, "Too long, max 15 letters!")
+    .required("This field is required!"),
+  email: Yup.string()
+    .email("Must be in email format!")
+    .required("This field is required!"),
+  role: Yup.string()
+    .oneOf(["guest", "user", "admin"])
+    .required("This field is required!"),
+  comment: Yup.string()
+    .max(100, "Too long, max 100 letters!")
+    .required("This field is required!"),
+  options: Yup.array().required("This field is required!"),
+});
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    onAdd({
-      username: event.target.elements.username.value,
-      role: event.target.elements.role.value,
-    });
-
-    event.target.reset();
+export default function UserForm() {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit}>
-      <div className={css.group}>
-        <label htmlFor={`${id}-username`}>Username:</label>
-        <input type="text" name="username" id={`${id}-username`} />
-      </div>
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        role: "guest",
+        comment: "",
+        options: [],
+      }}
+      validationSchema={UserSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.form}>
+        <div className={css.formGroup}>
+          <label>Username:</label>
 
-      <div className={css.group}>
-        <label htmlFor={`${id}-role`}>Role:</label>
-        <select name="role" id={`${id}-role`}>
-          <option value="guest">Guest</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
-        </select>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+          <Field type="text" className={css.input} name="username" />
+          <ErrorMessage
+            name="username"
+            component="span"
+            className={css.error}
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label>Email:</label>
+          <Field type="email" className={css.input} name="email" />
+          <ErrorMessage name="email" component="span" className={css.error} />
+        </div>
+
+        <div className={css.formGroup}>
+          <label>Role:</label>
+          <Field className={css.input} as="select" name="role">
+            <option value="guest">Guest</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </Field>
+          <ErrorMessage name="role" component="span" className={css.error} />
+        </div>
+
+        <div className={css.formGroup}>
+          <label>Comment:</label>
+          <Field as="textarea" className={css.input} name="comment"></Field>
+          <ErrorMessage name="comment" component="span" className={css.error} />
+        </div>
+
+        <div className={css.formGroup}>
+          <div>
+            <Field type="checkbox" name="options" value="a" /> A
+          </div>
+          <div>
+            <Field type="checkbox" name="options" value="b" /> B
+          </div>
+          <div>
+            <Field type="checkbox" name="options" value="c" /> C
+          </div>
+          <ErrorMessage name="options" component="span" className={css.error} />
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 }
